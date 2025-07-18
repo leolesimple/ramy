9feee710b33846d8c48e80815c614afbd2439d57
Léo Lesimple <leolesimple@icloud.com>
2025-07-18 17:02:17 +0200

    Update Button component link in page.tsx

--- app/menu/page.tsx
+++ app/menu/page.tsx
@@ -5,7 +5,7 @@ export default function MenuPage() {
         <div className="flex flex-col min-h-screen items-center justify-center bg-stone-950 text-stone-200 font-sans antialiased px-4 text-center space-y-8 overflow-hidden">
             <h2 className="text-4xl font-bold max-w-md">Choisissez une action à effectuer :</h2>
             <div className="flex gap-4 w-full max-w-fit mx-auto">
-                <Button href="/ajout">
+                <Button href="/lignes">
                     Ajout
                 </Button>
                 <Button href="/vision">
