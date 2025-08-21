73ce7cf8ede9b522d076b6d723f6b99ad781246c
Léo Lesimple <leolesimple@icloud.com>
2025-08-21 16:02:18 +0200

    :sparkles: (PWA) Integrate next-pwa for Progressive Web App support and update configuration

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
+import withPWA from 'next-pwa';
 
 const nextConfig: NextConfig = {
-
+    reactStrictMode: true,
+    // Tu peux ajouter ici d'autres options si tu veux
 };
 
-export default nextConfig;
+export default withPWA({
+    dest: 'public',
+    register: true,
+    skipWaiting: true,
+    disable: process.env.NODE_ENV === 'development', // pas de PWA en dev
+})(nextConfig);
