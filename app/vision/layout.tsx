6fc3ddad80c258e0594aa87ddeaa58585ef5596b
Léo Lesimple <leolesimple@icloud.com>
2025-08-23 19:17:05 +0200

    :bug: (vision/) - Double Nav Buttons fixed.

--- app/vision/layout.tsx
+++ app/vision/layout.tsx
@@ -7,10 +7,8 @@ export default function VisionLayout({ children }: { children: React.ReactNode }
     return (
         <div className="min-h-screen bg-slate-950 text-slate-200 font-sans antialiased flex flex-col dark:bg-slate-200 dark:text-slate-950">
             <ThemeProvider>
-                <ClientLayoutWrapper>
                     <ThemeToggle></ThemeToggle>
                     {children}
-                </ClientLayoutWrapper>
             </ThemeProvider>
         </div>
     );
